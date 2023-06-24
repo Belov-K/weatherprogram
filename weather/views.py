0258e5e076e991d67c43671bc578edba71fed67e
kiril <kaz1nak3@mail.ru>
2023-06-24 11:43:14 +0300

    feat(weather): добавил вывод текста

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,15 @@
+import requests
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'weather/index.html')
+    appid='7bb22b66e96d4bfba289943498c913e8'
+    url='https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city='London'
+    res=requests.get(url.format(city)).json()
+    city_info={
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"]
+    }
+    context={'info': city_info}
+    return render(request, 'weather/index.html', context)

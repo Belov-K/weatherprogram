13a17856ab3712f0f1662683726959dbcb9e6cda
kiril <kaz1nak3@mail.ru>
2023-06-26 12:21:59 +0300

    feat(weather): добавил вывод текста

--- weather/urls.py
+++ weather/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name='weather'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('info/', views.info, name='info'),
+    path('documents/', views.documents, name='documents'),
 ]
